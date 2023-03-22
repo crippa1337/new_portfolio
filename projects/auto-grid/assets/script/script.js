e54fa788911bdf68ebb969ea48badd16370cfd54
Cristopher T <cristopher.torgrip@elev.ga.ntig.se>
2023-03-22 11:48:08 +0100

    good animation handling on grid-example

--- projects/auto-grid/assets/script/script.js
+++ projects/auto-grid/assets/script/script.js
@@ -1,7 +1,20 @@
+// list of all the images
 const images = document.querySelectorAll(".image");
 
+// on all the images...
 images.forEach(image => {
-    image.addEventListener('click', (e) => {
-        image.classList.add('spin-image')
-    })
-})
+    // listen for click events
+    image.addEventListener('click', () => {
+        // if an image was clicked, add the spin-image class to it
+        // triggering the animation
+        image.classList.add('spin-image');
+    });
+
+    // listen for animations ending
+    image.addEventListener('animationend', () => {
+        // remove the spin-image class from the image so that it
+        // can be re-applied if clicked again. otherwise it would only be able
+        // to play the animation once
+        image.classList.remove('spin-image');
+    });
+});
